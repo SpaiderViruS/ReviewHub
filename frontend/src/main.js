17bc4e8575a8d385688e5967f5147c3bda0196ad
SpaiderViruS <emel07827@gmail.com>
2025-05-29 17:28:09 +0500

    feat(main.js): rework main logic

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,10 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 
 // Vuetify 3
-import { createVuetify } from 'vuetify';
-import 'vuetify/styles' 
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
+import vuetify from './plugins/vuetify'
 
 import App from './App.vue'
 import router from './router'
@@ -14,15 +11,10 @@ import router from './router'
 // Api
 import apiPlugin from './plugins/api.js';
 
-const vuetify = createVuetify({
-  components,
-  directives,
-})
-
 const app = createApp(App)
 
-app.use(router)
-app.use(vuetify)
+app.use(router);
+app.use(vuetify);
 app.use(apiPlugin);
 
 app.provide('$api', app.config.globalProperties.$api);
