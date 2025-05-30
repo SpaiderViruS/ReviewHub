8582bc88e7aad038e33c68f4c5b2afa518ed9941
SpaiderViruS <emel07827@gmail.com>
2025-05-30 15:05:32 +0500

    feat(router): new route catalog

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import MainComponent from '@/components/index.vue';
+import catalog from '@/components/catalog.vue';
 
 // 404
 import EmptyPage from '@/components/emptyPage.vue';
 
 const routes = [
   { path: '/', component: MainComponent },
+  { path: '/catalog', component: catalog },
 
   // 404
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: EmptyPage }
