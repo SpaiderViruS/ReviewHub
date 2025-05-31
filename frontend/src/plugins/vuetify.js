a779ecb0cf6b01a8983f81c63772520be348593f
SpaiderViruS <emel07827@gmail.com>
2025-05-31 19:41:11 +0500

    feat(vuetify): add mdi font

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,7 +1,9 @@
-import { createVuetify } from 'vuetify'
-import 'vuetify/styles'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+import '@mdi/font/css/materialdesignicons.css';
 
 import { mdi } from "vuetify/iconsets/mdi";
 import { customSVGs } from "@/plugins/customIcons";
