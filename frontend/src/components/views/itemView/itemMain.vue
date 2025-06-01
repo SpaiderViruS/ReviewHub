1e88fb64d4c0e26e792080b364696e84a6fd126c
SpaiderViruS <emel07827@gmail.com>
2025-06-01 15:50:19 +0500

    feat(itemMain): made into separate components

---
+++ frontend/src/components/views/itemView/itemMain.vue
@@ -0,0 +1,21 @@
+<template>
+  <itemDetails v-if="item"/>
+  <reviewSection v-if="item" :item-id="item.id"/>
+</template>
+<script setup>
+import { ref, onMounted, inject } from 'vue'
+import { useRoute } from 'vue-router'
+import itemDetails from './itemDetails.vue'
+import reviewSection from './reviewSection.vue'
+
+const route = useRoute()
+const $api = inject('$api')
+const item = ref(null)
+
+onMounted(async () => {
+  const res = await $api.get(`/item/${route.params.id}`);
+  item.value = res.data
+})
+</script>
+<style>
+</style>
