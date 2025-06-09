d5df054090b9213eceaf40fa714328ad1d1ca8c3
SpaiderViruS <emel07827@gmail.com>
2025-06-10 00:17:03 +0500

    feat(users.router): update routes for new method

--- backend/routes/users.router.js
+++ backend/routes/users.router.js
@@ -3,7 +3,13 @@ const router = new Router();
 
 const userController = require('../controllers/users.controller');
 
+const authMiddleware = require('../middleware/auth.middleware');
+
 router.post('/login', userController.login);
 router.post('/registration', userController.regisration);
+
+// Admin routes
+// TODO: сделать проверку на роль админа в токене, возможно сделать новый мидлвеер админский
+router.get('/all', authMiddleware, userController.getAllUsers);
 
 module.exports = router;
