5118a410eee93203fe9d14f0f640f8d55724c5e9
SpaiderViruS <emel07827@gmail.com>
2025-06-11 22:03:32 +0500

    feat(item.router): update routes

--- backend/routes/item.router.js
+++ backend/routes/item.router.js
@@ -3,8 +3,14 @@ const router = new Router();
 
 const itemController = require('../controllers/item.controller');
 
+const authMiddleware = require('../middleware/auth.middleware')
+
 router.get('/', itemController.getItems);
 router.get('/popular', itemController.getPopularsItems)
 router.get('/:id', itemController.getCurrentItem);
+
+// Admin routes
+router.post('/', authMiddleware, itemController.createItem);
+router.put('/:id', authMiddleware, itemController.editItem);
 
 module.exports = router;
