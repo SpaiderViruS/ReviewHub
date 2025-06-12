5c381bc2dc9e525f97db442afda660c69a6a594b
SpaiderViruS <emel07827@gmail.com>
2025-06-12 14:18:24 +0500

    feat(statistic): update middlewares

--- backend/routes/statistic.router.js
+++ backend/routes/statistic.router.js
@@ -2,10 +2,12 @@ const Router = require('express');
 const router = new Router();
 
 const authMiddleware = require('../middleware/auth.middleware');
+const adminMiddleware = require('../middleware/admin.middleware');
+
 const statisticContoller = require('../controllers/statistic.controller')
 
-router.get('/counts', authMiddleware, statisticContoller.getCounts);
-router.get('/top-rated', authMiddleware, statisticContoller.getTopRated);
-router.get('/by-year', authMiddleware, statisticContoller.getItemsByYear);
+router.get('/counts', authMiddleware, adminMiddleware, statisticContoller.getCounts);
+router.get('/top-rated', authMiddleware, adminMiddleware, statisticContoller.getTopRated);
+router.get('/by-year', authMiddleware, adminMiddleware, statisticContoller.getItemsByYear);
 
 module.exports = router;
