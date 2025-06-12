e0349a83f98f8b65c0de89b141b9987d551d7ba0
SpaiderViruS <emel07827@gmail.com>
2025-06-12 14:18:37 +0500

    feat(users.router): update middlewares

--- backend/routes/users.router.js
+++ backend/routes/users.router.js
@@ -4,14 +4,15 @@ const router = new Router();
 const userController = require('../controllers/users.controller');
 
 const authMiddleware = require('../middleware/auth.middleware');
+const adminMiddleware = require('../middleware/admin.middleware');
 
 router.post('/login', userController.login);
 router.post('/registration', userController.regisration);
 
 // Admin routes
 // TODO: сделать проверку на роль админа в токене, возможно сделать новый мидлвеер админский
-router.get('/all', authMiddleware, userController.getAllUsers);
-router.put('/:id', authMiddleware, userController.editUser);
-router.delete('/:id', authMiddleware, userController.deleteUser);
+router.get('/all', authMiddleware, adminMiddleware, userController.getAllUsers);
+router.put('/:id', authMiddleware, adminMiddleware, userController.editUser);
+router.delete('/:id', authMiddleware, adminMiddleware, userController.deleteUser);
 
 module.exports = router;
