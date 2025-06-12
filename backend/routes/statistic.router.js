a0e0add29d7da03bae97debf9191fc623e82f1a6
SpaiderViruS <emel07827@gmail.com>
2025-06-13 00:34:13 +0500

    faet(stats): update routes

--- backend/routes/statistic.router.js
+++ backend/routes/statistic.router.js
@@ -9,5 +9,8 @@ const statisticContoller = require('../controllers/statistic.controller')
 router.get('/counts', authMiddleware, adminMiddleware, statisticContoller.getCounts);
 router.get('/top-rated', authMiddleware, adminMiddleware, statisticContoller.getTopRated);
 router.get('/by-year', authMiddleware, adminMiddleware, statisticContoller.getItemsByYear);
+router.get('/most-disc', authMiddleware, adminMiddleware, statisticContoller.mostDiscosing);
+router.get('/most-favorite', authMiddleware, adminMiddleware, statisticContoller.getMostFavorites);
+router.get('/counting-types', authMiddleware, adminMiddleware, statisticContoller.typesCountForDiagramm);
 
 module.exports = router;
