323c7595bc37f22ba63dbda7fb5828671e02b813
SpaiderViruS <emel07827@gmail.com>
2025-06-12 23:25:06 +0500

    feat(reviews.controller): add new method and use prettier

--- backend/controllers/reviews.controller.js
+++ backend/controllers/reviews.controller.js
@@ -1,4 +1,4 @@
-const db = require('../db');
+const db = require("../db");
 
 class reviewController {
   async getItemReviews(req, res, next) {
@@ -20,7 +20,8 @@ class reviewController {
           LEFT JOIN ratings AS rtg ON rtg.user_id = rv.user_id AND rtg.item_id = rv.item_id
           WHERE rv.item_id = $1
           ORDER BY rv.created_at;
-        `, [ itemId ]
+        `,
+        [itemId]
       );
 
       res.status(200).json(data.rows);
@@ -34,7 +35,8 @@ class reviewController {
       const { rating, text, item_id } = req.body;
       const user_id = req.user.id;
 
-      if (!rating || !text || !user_id || !item_id) throw new Error("Недостаточно данных");
+      if (!rating || !text || !user_id || !item_id)
+        throw new Error("Недостаточно данных");
 
       const existingReview = await db.query(
         `SELECT * FROM reviews WHERE user_id = $1 AND item_id = $2`,
@@ -65,7 +67,32 @@ class reviewController {
         );
       }
 
-      res.status(201).json("OK")
+      res.status(201).json("OK");
+    } catch (err) {
+      res.status(400).json(err.message);
+    }
+  }
+
+  async getUserReview(req, res, next) {
+    try {
+      const id = req.user.id;
+
+      if (!id) throw new Error("Недостаточно данных");
+
+      const data = await db.query(
+        `
+            SELECT 
+              i.title AS item_title,
+              r.text,
+              r.created_at
+            FROM reviews AS r
+            JOIN items AS i ON i.id = r.item_id
+            WHERE r.user_id = $1
+          `,
+        [id]
+      );
+
+      res.status(200).json(data.rows);
     } catch (err) {
       res.status(400).json(err.message);
     }
